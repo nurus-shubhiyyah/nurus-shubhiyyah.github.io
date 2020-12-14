5878c2bca4ee5262ce501390b7e2740b9d9b7e2a
nurus-shubhiyyah <71313999+nurus-shubhiyyah@users.noreply.github.com>
2020-12-15 01:56:12 +0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var n = prompt("안녕하새요^^, 당신의 이름을 (NAME) 입력 주세요: ");
+var c = confirm("Hello " + n + ", WELCOME TO KPOP MERCH AREA");
+if (c == true) {
+    alert('LETS BUY SOME MERCH');
+} else {
+    alert('No Problem');
+}
